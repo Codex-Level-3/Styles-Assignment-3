a0f84827269a20d3a5167feeef0be6d65cf0d031
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-04-11 00:10:54 -0500

    created a component utilizing tailwind css class

---
+++ src/app/components/Hero.jsx
@@ -0,0 +1,19 @@
+"use client";
+
+export default function Hero() {
+  return (
+    <div id="hero" style={{ width: "100vw", height: "350px" }}>
+      <div className="bg-[url('/solar.jpg')] h-full bg-cover justify-center content-center">
+        <div className="backdrop-blur-sm p-4 flex flex-col items-center">
+          <h1 className="text-white text-3xl font-bold text-center">Planets</h1>
+          <p className="text-white text-2xl mb-2">
+            Let's explore our solar system! Discover planet facts!
+          </p>
+          <button className="text-white text-xl bg-blue-500 hover:bg-cyan-600 p-2 rounded font-semibold">
+            Explore!
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
